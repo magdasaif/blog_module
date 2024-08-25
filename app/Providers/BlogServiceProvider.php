18404051c18a965ee7c32b0ab45677d7c18d5e14
magda <magdasaif3@gmail.com>
2024-08-25 13:15:26 +0300

    handle blog command to publish module folders and handle status also

--- app/Providers/BlogServiceProvider.php
+++ app/Providers/BlogServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Blog\Providers;
 
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -31,41 +30,7 @@ class BlogServiceProvider extends ServiceProvider
             dirname(__DIR__) .'/..' => base_path('Modules/'.$this->moduleName)        
         ], $this->moduleNameLower.'-module');
         //==============================================================================================
-        $this->handleModulesStatusJsonFile();
-        //==============================================================================================
-    }
-    //==============================================================================================
-    public function handleModulesStatusJsonFile(){
-        //==============================================================================================
-        // Check if modules_statuses.json exists
-        $statusesFilePath = base_path('modules_statuses.json');
-        $add_attribute=false;
-        if (File::exists($statusesFilePath)) {
-            $statuses = json_decode(File::get($statusesFilePath), true);
-            //==============================================================================================
-            $json = file_get_contents($statusesFilePath); // or json string
-            $data = json_decode($json, true);
-        
-            if (\Illuminate\Support\Arr::has($data, $this->moduleName)) {
-                // return 'key exists do something';
-            } else {
-                // return ' key DOES NOT exist do something else';
-                $add_attribute=true;
-            }
-            //==============================================================================================
-        } else {
-            $statuses = [];
-            $add_attribute=true;
-        }
-        //==============================================================================================
-        if($add_attribute){
-            // Update the statuses based on the package being published
-            $package = $this->moduleName; // Change this dynamically based on your package
-            $statuses[$package] = true;
-        }
-        //==============================================================================================
-        // Write the updated statuses back to modules_statuses.json
-        File::put($statusesFilePath, json_encode($statuses, JSON_PRETTY_PRINT));
+        // $this->handleModulesStatusJsonFile();
         //==============================================================================================
     }
     //==============================================================================================
@@ -83,7 +48,10 @@ class BlogServiceProvider extends ServiceProvider
      */
     protected function registerCommands(): void
     {
-        // $this->commands([]);
+        $this->commands([
+            // \Modules\Blog\Console\PublishModuleCommand::class,
+        ]);
+
     }
     //==============================================================================================
     /**
